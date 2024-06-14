58375589c28eb87b248a42c5b6ca8b840944a6d4
Joy Wang <yihan.wang3@ucalgary.ca>
2024-06-13 21:50:59 -0600

    delete commented line

--- src/Client/client_main.cpp
+++ src/Client/client_main.cpp
@@ -15,7 +15,6 @@ string Hash(string password) {
     py::bytes bPassword = py::bytes(password); // Password
     sha256.attr("update")(bPassword);
     py::str hex = sha256.attr("hexdigest")();
-    //py::print(hex);
     auto hashedPassword = hex.cast<std::string>();
     //std::cout << message;
     return hashedPassword;
